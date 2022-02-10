7e604a47f412b09f7b5536deacda8367f180acd8
BrainJarProductions <vgrilli1@gmail.com>
2022-02-10 10:52:27 -0500

    fix manifest logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/temp-logo.png`,
+        icon: `src/images/griffin-logo-sm.jpg`,
       },
     },
     {
